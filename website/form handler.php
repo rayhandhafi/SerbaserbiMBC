f800736d89ae310d0b354da5922d6d15ee28d037
rayhandhafi <rayhandhafi21@gmail.com>
2022-11-22 16:23:54 +0700

    updated form handler.php

--- website/form handler.php
+++ website/form handler.php
@@ -18,7 +18,7 @@ $headers = "From: $email_from \r\n";
 $headers = "Reply-To: $visitor_email \r\n";
 
 mail($to, $email_subject, $email_body, $headers)
-header("Location: contact.html");
+header("Location: contact us.html");
 
 
 
